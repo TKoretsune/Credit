de39840fcb2e7a72d6495fa95334455be0fb6942
TakehiroKoretsune <oyayubi.cocacola@gmail.com>
2014-06-20 09:45:48 +0900

    Initial Commit

---
+++ CreditTests/CreditTests.m
@@ -0,0 +1,34 @@
+//
+//  CreditTests.m
+//  CreditTests
+//
+//  Created by Takehiro Koretsune on 2014/06/20.
+//  Copyright (c) 2014年 是常 雄大. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CreditTests : XCTestCase
+
+@end
+
+@implementation CreditTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
